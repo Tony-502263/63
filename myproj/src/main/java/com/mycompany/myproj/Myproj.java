4586777d75fb3a75d269e7e8dac0d9a5bc263156
TONY STARK <tony.502263@sxcce.edu.in>
2025-05-10 10:04:46 +0530

    Add files via upload

---
+++ myproj/src/main/java/com/mycompany/myproj/Myproj.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.myproj;
+
+/**
+ *
+ * @author Tony Mathew
+ */
+public class Myproj {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
